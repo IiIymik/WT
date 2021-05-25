c529e206c014d17a2f52ce058fe8d9c0b93f4655
IiIymik <71165032+IiIymik@users.noreply.github.com>
2021-05-25 23:07:55 +0300

    done for North and South

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ const ctx = document.getElementById('myChart').getContext('2d');
 const GLOBAL_MIN_TENPERATURE = 14;
 
 
-fetchData().then(parseData).then(getLabelsAndData).then(({years, temps})=> drawChart(years, temps));
+fetchData().then(parseData).then(getLabelsAndData).then(({years, tempsG, tempsN, tempsS})=> drawChart(years, tempsG, tempsN, tempsS));
 
 function fetchData() {
     return fetch("../ZonAnn.Ts+dSST.csv").then(res => res.text());
@@ -13,52 +13,58 @@ function parseData(data) {
 };
 
 function getLabelsAndData(data) {
+    console.log(data)
     return data.reduce(
                 (acc, entry) => {
                     acc.years.push(entry.Year);
-                    acc.temps.push(Number(entry.Glob) + GLOBAL_MIN_TENPERATURE);
-
+                    acc.tempsG.push(Number(entry.Glob) + GLOBAL_MIN_TENPERATURE);
+                    acc.tempsN.push(Number(entry.NHem) + GLOBAL_MIN_TENPERATURE);
+                    acc.tempsS.push(Number(entry.SHem) + GLOBAL_MIN_TENPERATURE);
                     return acc;
                 },
-                { years: [], temps: [] }
+                { years: [], tempsG: [], tempsN: [], tempsS: [] }
             );
 };
 
-function drawChart(labels, data) {
+function drawChart(labels, tempsG, tempsN, tempsS) {
         new Chart(ctx, {
     type: 'line',
-    data: {
-        labels,
-        datasets: [{
-            label: 'Средняя температура планеты',
-            data,
-            backgroundColor:'blue',
-            borderColor:'blue',
-            borderWidth: 1,
-            fill: false,
-        }]
-    },
+            data: {
+                labels,
+                datasets: [{
+                    label: 'Средняя температура планеты',
+                    data: tempsG,
+                    backgroundColor: 'blue',
+                    borderColor: 'blue',
+                    borderWidth: 1,
+                    fill: false,
+                }, {
+                    label: 'Средняя Северная температура планеты',
+                    data: tempsN,
+                    backgroundColor: 'green',
+                    borderColor: 'green',
+                    borderWidth: 1,
+                    fill: false,
+                    }, {
+                     label: 'Средняя Южная температура планеты',
+                    data: tempsS,
+                    backgroundColor: 'red',
+                    borderColor: 'red',
+                    borderWidth: 1,
+                    fill: false,
+                }]
+            },        
     options: {
         scales: {
             yAxes: {
                 ticks: {
                     callback(value) {
-                        return value + "$";
+                        return value + "💥";
                     }
                 },
             }
         }
     }
-});
+        });
 }
-        
-
-
-
-
-
-
-           
-
-
-        
+    
